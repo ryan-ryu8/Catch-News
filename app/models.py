5b7eb29e22bf216668ecb55db0907a77ead849a3
Munge Ryan <mungeryan@gmail.com>
2020-07-05 14:44:00 +0300

    Adding class headlines

--- app/models.py
+++ app/models.py
@@ -26,5 +26,21 @@ class Source:
     '''
     Define source objects
     '''
-    def __init__
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+
+class Headlines:
+    '''
+    Instantiates objects of headlines review objects of the news
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
     
