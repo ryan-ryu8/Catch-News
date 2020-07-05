6ff4a579c821342412599cc1f6fcbd1b8ac5d58a
Munge Ryan <mungeryan@gmail.com>
2020-07-05 14:29:37 +0300

    Adding news object

---
+++ app/models.py
@@ -0,0 +1,30 @@
+class News:
+    '''
+    Instantiates objects of article news of news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class New_review:
+    '''
+    Instantiates objects of the news review of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Source:
+    '''
+    Define source objects
+    '''
+    def __init__
+    
