d465d0119c4dd0da0c057297b827d5dcf841ea88
Munge Ryan <mungeryan@gmail.com>
2020-07-06 22:40:40 +0300

    Importing the render_template

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from flask import render_template,request,redirect,url_for
-from ..request import get_source,article_source,get_category,get_headlines
+from ..request import get_source,news_source,get_category,get_headlines
 
 @main.route('/')
 def index():
