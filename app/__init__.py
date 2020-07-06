4d77c7ecaf2921f49a811e45172326dabfbf57be
Munge Ryan <mungeryan@gmail.com>
2020-07-06 22:38:48 +0300

    Removing the config_reuest from request file

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,4 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .request import config_request
-    config_request(app)
-
     return app
