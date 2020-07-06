177b9d9fbbc020617fe6391f64088b6ddd0483ee
Munge Ryan <mungeryan@gmail.com>
2020-07-06 12:39:22 +0300

    Registering blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,11 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .request import config_request
+    config_request(app)
+
+    return app
