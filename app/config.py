c96cdd2cba3586491d21ecbb2d5056461f80bb40
Munge Ryan <mungeryan@gmail.com>
2020-07-06 12:20:56 +0300

    Adding a parent class configuration

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import is 
+
+class Config:
+    '''
+    Parent class configuration
+    '''
+    News_Api_Source_url = 'https://newsapi/headlines?sources={}&apiKey={}'
+    Review_Api_url = 'https://newsapi/headlines?country=ke&apiKey={}'
