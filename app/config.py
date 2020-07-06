871832ca3c03017853fe5dd15a6fb24cca28650c
Munge Ryan <mungeryan@gmail.com>
2020-07-06 12:27:24 +0300

    Adding a parent class configuration

--- app/config.py
+++ app/config.py
@@ -6,3 +6,25 @@ class Config:
     '''
     News_Api_Source_url = 'https://newsapi/headlines?sources={}&apiKey={}'
     Review_Api_url = 'https://newsapi/headlines?country=ke&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    Child class config
+    Args:
+    Config: Parent config with general config settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Child class development
+    Args:
+    Config:  Parent config with general config settings
+    '''
+    
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
