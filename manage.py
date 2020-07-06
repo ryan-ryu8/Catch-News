9fd96ca48b67ca8582ba11830cf5485b4d509aca
Munge Ryan <mungeryan@gmail.com>
2020-07-06 14:43:38 +0300

    Changing name from run to manage

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import creat_app
+from flask_script import Manager,Server
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """
+    Run unittest
+    """
+    import unittest
+    test = unittest.TestLoader().discover('test')
+    unittest.TestTestRunner(verbosity=2).run(tests)
+
+if __name__ = '__main__':
+    manager.run
