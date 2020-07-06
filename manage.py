657fd6cc405f6206733e4b542b8c82284d2fb00a
Munge Ryan <mungeryan@gmail.com>
2020-07-06 22:20:14 +0300

    Calling the run method to run the application

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import creat_app
+from app import create_app
 from flask_script import Manager,Server
 
 app = create_app('development')
@@ -15,5 +15,5 @@ def test():
     test = unittest.TestLoader().discover('test')
     unittest.TestTestRunner(verbosity=2).run(tests)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     manager.run
